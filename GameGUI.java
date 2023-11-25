6ad04a037fd8cb7bd10dddb4f63e2bf4694753e6
gabby peña <gpena127@gwu.edu>
2023-11-25 16:41:14 -0500

    Pseudocode/Comments

--- GameGUI.java
+++ GameGUI.java
@@ -1,1 +1,22 @@
 //GUI for the game
+
+
+//pseudo code for GUI - done by gabby 
+/*
+ *  JFrame labeled "TicTacToe"
+ * 
+ *  mainPanel - contains board
+ * make all 9 parts of the board clickable
+ *  when O clicks on a blank, it turns into an O
+ *  when X clicks on a blank, it turns into an X
+ *  after the user clicks that area becomes NON EDITABLE
+ * 
+ * option button QUIT to end game
+ * 
+ * bottomPanel - JTextfield with instructions
+ * 
+ * display Player's symbol in bottom right corner
+ * 
+ * status changes of player's turn
+ * 
+ */
