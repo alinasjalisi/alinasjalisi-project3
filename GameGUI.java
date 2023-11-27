c048c9dd27ad6054dfc32560c219ffa53732757f
gabby peña <gpena127@gwu.edu>
2023-11-27 10:20:14 -0500

    bottomPanel - gabby

--- GameGUI.java
+++ GameGUI.java
@@ -10,7 +10,7 @@ public class GameGUI extends JFrame
     //top buttons used to connect to the server
     private JTextField IP_Address;
     private JTextField PortNum;
-    private JTextField HowTo; //instructions for game
+    private JTextArea HowTo; //instructions for game
     private JButton Connect_Disconnect;
 
     private JButton New_Game;
@@ -30,12 +30,7 @@ public class GameGUI extends JFrame
 
     public GameGUI()
     {
-
-
         super("TicTacToe");
-
-        
-
         //used to connect with the server methods
         startGameGUI();
     }
@@ -48,8 +43,6 @@ public class GameGUI extends JFrame
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
                 
-    
-
         //setting the top panel with name, ip, port, and connect
         JPanel topPanel = new JPanel(new FlowLayout());
         
@@ -71,95 +64,19 @@ public class GameGUI extends JFrame
         topPanel.add(Connect_Disconnect);
 
         //adding instructions how to play the game at the bottom
-        JPanel bottomPanel = new JPanel();
-        HowTo = new JTextField(20);
-        //needs to be at the bottom of JFrame
+        JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+        JLabel howToLabel = new JLabel("How to Play: ");
+        bottomPanel.add(howToLabel);
+        HowTo = new JTextArea(); 
+        HowTo.setPreferredSize(new Dimension(700, 50));
         bottomPanel.add(HowTo);
-
-        // alina - not sure if this works but i tried lol: 
-    // Create a panel for the status and TicTacToe board
-    private JPanel createMiddlePanel() {
-    JPanel middlePanel = new JPanel(new GridLayout(2, 1));
-
-    // Create a panel for the TicTacToe board
-    JPanel boardPanel = new JPanel(new GridLayout(3, 3));
-
-    // Create nine JButtons for the TicTacToe board cells
-    JButton[][] boardButtons = new JButton[3][3];
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            boardButtons[i][j] = new JButton();
-            boardButtons[i][j].addActionListener(new BoardButtonActionListener(i, j));
-            boardPanel.add(boardButtons[i][j]);
-        }
-    }
-
-    // Create a JTextArea to display the status
-    JTextArea statusTextArea = new JTextArea();
-    statusTextArea.setEditable(false);
-    statusTextArea.setFont(new Font(Font.SERIF, Font.PLAIN, 24));
-    statusTextArea.setText("Player X's turn");
-
-    // Add the board panel and status text area to the middle panel
-    middlePanel.add(boardPanel);
-    middlePanel.add(statusTextArea);
-
-    return middlePanel;
-}
-
-    private JPanel createRightPanel() {
-    JPanel rightPanel = new JPanel(new GridLayout(4, 1));
-
-    // Create a JLabel for the "Options" section
-    JLabel optionsLabel = new JLabel("Options");
-    rightPanel.add(optionsLabel);
-
-    // Create a JButton to start a new game
-    JButton newGameButton = new JButton("New Game");
-    newGameButton.addActionListener(new NewGameButtonActionListener());
-    rightPanel.add(newGameButton);
-
-    // Create a JButton to quit the current game
-    JButton quitGameButton = new JButton("Quit Game");
-    quitGameButton.addActionListener(new QuitGameButtonActionListener());
-    rightPanel.add(quitGameButton);
-
-    // Create a JLabel for the "Your symbol is" section
-    JLabel yourSymbolLabel = new JLabel("Your symbol is:");
-    rightPanel.add(yourSymbolLabel);
-
-    // Create a JTextArea to display the user's symbol
-    JTextArea userSymbolTextArea = new JTextArea();
-    userSymbolTextArea.setEditable(false);
-    // Need to call a method from a different class to get the user's symbol
-    userSymbolTextArea.setText("X");
-    //maybe an if statement changing it to userSymbolTextArea.setText("O"); if the method that sets user symbols already assigned player 1 X
-
-
-
-    // Add the user symbol text area to the right panel
-    rightPanel.add(userSymbolTextArea);
-
-    return rightPanel;
-}
-
-// Create a panel to display the last move
-private JPanel createLeftPanel() {
-    JPanel leftPanel = new JPanel(new FlowLayout());
-
-    // Create a JTextArea to display the last move message
-    JTextArea lastMoveTextArea = new JTextArea();
-    lastMoveTextArea.setEditable(false);
-    // Need to call a method from the LastMove class to get the last move message
-    lastMoveTextArea.setText("Player X made the last move at A1");
-    //^^need to fix cuz it should be calling on lastmove.java class to update the text here ^^
-
-    // Add the last move text area to the left panel
-    leftPanel.add(lastMoveTextArea);
-
-    return leftPanel;
-}
-
+        HowTo.setText("You will be randomly assigned a symbol: 'X' or 'O'.\n"
+        + "Click on one of the buttons on the board to make your move.\n"
+        + "To win, create a column, row, or diagonal pattern with your symbol before the other player can.\n"
+        + "A draw will only occur when the entire board is used up and neither player was able to create one of the patterns.");
+        HowTo.setEditable(false);
+        add(bottomPanel, BorderLayout.SOUTH);
     }
 
     public static void main(String[] args) 
